d2bc97073625720372bb975da29e850942cbffcb
Yerassyl Maratov <maratov.2003@gmail.com>
2025-04-02 19:25:53 +0500

    project init

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'your_password',
+      database: 'ufc_db',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true, // Set to false in production
+    }),
+    // Add other modules here
+  ],
+})
+export class AppModule {}
